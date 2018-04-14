004f94fab5aea87f56ee20965d2bedf53ae92c63
Eric Cannon <ericcannon@Erics-MacBook-Pro-2.local>
2018-04-14 09:52:25 -0400

    Constuctor Hangman

---
+++ list.js
@@ -0,0 +1,6 @@
+
+//An array of words that'll be used in hangman.
+exports.newWord = {
+  wordList: [ "alligator", "giraffe", "tiger", "falcon", "gorilla",
+              "leopard", "elephant", "sloth" ]
+};
